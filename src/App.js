1ee3296267301687857ae08890d9fb9dd1333da3
abhisn15 <abhinugroho41@smp.belajar.id>
2023-04-29 04:53:17 +0700

    sudah semua tinggal barcodenya dan juga memvalidasikan jika jawaban semua sudah terjawab maka sweet alert selamat akan muncull

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Swal from 'sweetalert2';
 import Typewriter from "typewriter-effect";
 import TextField from '@mui/material/TextField';
 import { Fragment } from "react";
@@ -8,8 +9,16 @@ import { OutlinedInput } from "@mui/material";
 
 export default function App() {
   const [inputValue, setInputValue] = useState('');
-  const [boxes, setBoxes] = useState(['', '', '', '']);
-  const [boxes2, setBoxes2] = useState(['', '', '', '']);
+  const [boxes, setBoxes] = useState(['', '', '', '', '', '', '']);
+  const [boxes2, setBoxes2] = useState(['', '', '', '', '']);
+  const [boxes3, setBoxes3] = useState(['', '', '', '']);
+  const [boxes4, setBoxes4] = useState(['', '', '', '', '']);
+  const [boxes5, setBoxes5] = useState(['', '', '', '']);
+  const [boxes6, setBoxes6] = useState(['', '', '']);
+  const [boxes7, setBoxes7] = useState(['', '', '', '', '']);
+  const [boxes8, setBoxes8] = useState(['', '', '']);
+  const [boxes9, setBoxes9] = useState(['', '', '', '']);
+  const [boxes10, setBoxes10] = useState(['', '', '', '', '']);
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -17,32 +26,174 @@ export default function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const answer = 'abhi'; // jawaban yang benar
-    const answer2 = 'apel'; // jawaban yang benar
-    if (inputValue.toLowerCase() === answer) {
-      const newBoxes = answer.split('').map((char) => char.toUpperCase());
-      setBoxes(newBoxes);
+    const answer = 'victory'; // jawaban yang benar
+    const answer2 = 'telur'; // jawaban yang benar
+    const answer3 = 'roda'; // jawaban yang benar
+    const answer4 = 'alien'; // jawaban yang benar
+    const answer5 = 'apel'; // jawaban yang benar
+    const answer6 = 'pir'; // jawaban yang benar
+    const answer7 = 'macan'; // jawaban yang benar
+    const answer8 = 'air'; // jawaban yang benar
+    const answer9 = 'paku'; // jawaban yang benar
+    const answer10 = 'racun'; // jawaban yang benar
+
+    const trimmedInputValue = inputValue.trim(); // trim whitespace pada input
+
+    if (trimmedInputValue.toLowerCase() === answer) {
+      const jawaban = answer.toUpperCase().split('');
+      setBoxes(jawaban);
+    }
+
+    if (trimmedInputValue.toLowerCase() === answer2) {
+      const jawaban = answer2.toUpperCase().split('');
+      setBoxes2(jawaban);
+    }
+
+    if (trimmedInputValue.toLowerCase() === answer3) {
+      const jawaban = answer3.toUpperCase().split('');
+      setBoxes3(jawaban);
+    }
+
+    if (trimmedInputValue.toLowerCase() === answer4) {
+      const jawaban = answer4.toUpperCase().split('');
+      setBoxes4(jawaban);
+    }
+
+    if (trimmedInputValue.toLowerCase() === answer5) {
+      const jawaban = answer5.toUpperCase().split('');
+      setBoxes5(jawaban);
+    }
+
+    if (trimmedInputValue.toLowerCase() === answer6) {
+      const jawaban = answer6.toUpperCase().split('');
+      setBoxes6(jawaban);
+    }
+
+    if (trimmedInputValue.toLowerCase() === answer7) {
+      const jawaban = answer7.toUpperCase().split('');
+      setBoxes7(jawaban);
+    }
+
+    if (trimmedInputValue.toLowerCase() === answer8) {
+      const jawaban = answer8.toUpperCase().split('');
+      setBoxes8(jawaban);
+    }
+
+    if (trimmedInputValue.toLowerCase() === answer9) {
+      const jawaban = answer9.toUpperCase().split('');
+      setBoxes9(jawaban);
+    }
+
+    if (trimmedInputValue.toLowerCase() === answer10) {
+      const jawaban = answer10.toUpperCase().split('');
+      setBoxes10(jawaban);
     }
     setInputValue('');
-    if (inputValue.toLowerCase() === answer2) {
-      const newBoxes2 = answer2.split('').map((char) => char.toUpperCase());
-      setBoxes2(newBoxes2);
-    }
-    setInputValue('');
-  };
+
+    if (inputValue === answer) {
+      // Menampilkan SweetAlert
+      Swal.fire({
+        title: 'Pesan',
+        text: 'Nilai input adalah tertentu!',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
+    };
+    if (inputValue === answer2) {
+      // Menampilkan SweetAlert
+      Swal.fire({
+        title: 'Pesan',
+        text: 'Nilai input adalah tertentu!',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
+    };
+    if (inputValue === answer3) {
+      // Menampilkan SweetAlert
+      Swal.fire({
+        title: 'Pesan',
+        text: 'Nilai input adalah tertentu!',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
+    };
+    if (inputValue === answer4) {
+      // Menampilkan SweetAlert
+      Swal.fire({
+        title: 'Pesan',
+        text: 'Nilai input adalah tertentu!',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
+    };
+    if (inputValue === answer5) {
+      // Menampilkan SweetAlert
+      Swal.fire({
+        title: 'Pesan',
+        text: 'Nilai input adalah tertentu!',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
+    };
+    if (inputValue === answer6) {
+      // Menampilkan SweetAlert
+      Swal.fire({
+        title: 'Pesan',
+        text: 'Nilai input adalah tertentu!',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
+    };
+    if (inputValue === answer7) {
+      // Menampilkan SweetAlert
+      Swal.fire({
+        title: 'Pesan',
+        text: 'Nilai input adalah tertentu!',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
+    };
+    if (inputValue === answer8) {
+      // Menampilkan SweetAlert
+      Swal.fire({
+        title: 'Pesan',
+        text: 'Nilai input adalah tertentu!',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
+    };
+    if (inputValue === answer9) {
+      // Menampilkan SweetAlert
+      Swal.fire({
+        title: 'Pesan',
+        text: 'Nilai input adalah tertentu!',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
+    };
+    if (inputValue === answer10) {
+      // Menampilkan SweetAlert
+      Swal.fire({
+        title: 'Pesan',
+        text: 'Nilai input adalah tertentu!',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
+    };
+  }
 
   return (
     <div>
       <Navbar className="relative mx-auto max-w-screen py-2 px-4 border-4 h-32 rounded-none rounded-b-[176px] max-[600px]:rounded-b-[10px] max-[600px]:h-20 shadow-md border-cyan-400 box-border lg:px-8 lg:py-4" >
         <div className="relative left-[106px] max-[600px]:left-5 max-[600px]:bottom-2 mx-auto items-center justify-between font-extrabold text-3xl leading-9">
-          <h1 className="title relative max-[600px]:w-[600px] max-[600px]:text-[18px] max-[400px]:top-2 max-[400px]:right-[28px] max-[400px]:w-[400px] max-[400px]:text-[11px] text-black">Ayo Mainkan Teka-Teki Silang yang Menyenangkan ini!</h1>
-          <p className="relative left-28 top-3 font-normal font-medium text-base max-[600px]:text-[11px] max-[600px]:left-4 max-[600px]:top-0 max-[400px]:w-[400px] max-[400px]:text-[7px] max-[400px]:left-[-15px] max-[400px]:top-0 max-[400px]:font-bold text-black">Perhatikan soal soal tersebut dan tulis jawabannya di kolom bawah Jawabannya adalah.</p>
+          <h1 className="title relative max-[600px]:w-[600px] max-[600px]:text-[18px] max-[400px]:top-2 max-[400px]:w-[400px] max-[400px]:text-[12px] text-black">Ayo Mainkan Teka-Teki Silang yang Menyenangkan ini!</h1>
+          <p className="relative left-28 top-3 font-normal font-medium text-base max-[600px]:text-[11px] max-[600px]:left-4 max-[600px]:top-0 max-[400px]:w-[400px] max-[400px]:text-[8px] max-[400px]:left-[8px] max-[400px]:top-0 max-[400px]:font-bold text-black">Perhatikan soal soal tersebut dan tulis jawabannya di kolom bawah Jawabannya adalah.</p>
         </div>
         <MobileNav>
         </MobileNav>
       </Navbar>
-      <div className="flex justify-between">
-        <div className="w-[600px] ml-20 mt-10 pb-40">
+      <div className="max-[2660px]:flex max-[600px]:flex-wrap-reverse">
+        <div className="w-[600px] max-[600px]:w-[350px] max-[600px]:ml-6 max-[600px]:mt-[-20px] ml-20 mt-10 pb-40">
           <p>1. Nama lain dari kata menang dalam permainan Mobile Legend adalah?<br /> <br />
             2. ..... Mata Sapi.<br /> <br />
             3. Benda bulat berputar yang terdapat dalam kendaraan adalah?<br /> <br />
@@ -55,8 +206,11 @@ export default function App() {
             10. Apa nama benda atau zat yang biasanya digunakan untuk membunuh atau mengendalikan hama tanaman atau serangga yang merusak?<br /> <br />
           </p>
         </div>
-        <div className="container pl-8 pt-4 pb-32 max-[1640px]:w-[684px] max-[600px]:w-[300px]">
+        <div className="container pl-10 pt-7 pb-32 max-[600px]:mt-2 max-[1640px]:w-[684px] max-[600px]:w-[300px]">
           <div className="flex max-[600px]:mt-10">
+            <div className="flex mt-2 items-center justify-center w-6 h-6 border-2 border-cyan-500 rounded-full">
+              <span className="text-black font-bold">1</span>
+            </div>
             {boxes.map((box, index) => (
               <div
                 key={index}
@@ -66,8 +220,115 @@ export default function App() {
               </div>
             ))}
           </div>
-          <div className="flex justify-center ml-[-400px]">
+          <div className="flex justify-center ml-[-260px] max-[600px]:ml-[-27px]">
+            <div className="flex mr-2 mt-2 items-center justify-center w-6 h-6 border-2 border-cyan-500 rounded-full">
+              <span className="text-black font-bold">2</span>
+            </div>
             {boxes2.map((box, index) => (
+              <div
+                key={index}
+                className="border border-gray-400 rounded-md w-10 h-10 flex items-center justify-center"
+              >
+                <p className="text-xl">{box}</p>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center ml-[-380px] max-[600px]:ml-[40px]">
+            <div className="flex mr-2 mt-2 items-center justify-center w-6 h-6 border-2 border-cyan-500 rounded-full">
+              <span className="text-black font-bold">3</span>
+            </div>
+            {boxes3.map((box, index) => (
+              <div
+                key={index}
+                className="border border-gray-400 rounded-md w-10 h-10 flex items-center justify-center"
+              >
+                <p className="text-xl">{box}</p>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center mb-5 ml-[-260px] max-[600px]:ml-[40px]">
+            <div className="flex mr-2 mt-2 items-center justify-center w-6 h-6 border-2 border-cyan-500 rounded-full">
+              <span className="text-black font-bold">4</span>
+            </div>
+            {boxes4.map((box, index) => (
+              <div
+                key={index}
+                className="border border-gray-400 rounded-md w-10 h-10 flex items-center justify-center"
+              >
+                <p className="text-xl">{box}</p>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center ml-[-220px] max-[600px]:ml-[40px]">
+            <div className="flex mr-2 mt-2 items-center justify-center w-6 h-6 border-2 border-cyan-500 rounded-full">
+              <span className="text-black font-bold">5</span>
+            </div>
+            {boxes5.map((box, index) => (
+              <div
+                key={index}
+                className="border border-gray-400 rounded-md w-10 h-10 flex items-center justify-center"
+              >
+                <p className="text-xl">{box}</p>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center ml-[-180px] max-[600px]:ml-[40px]">
+            <div className="flex mr-2 mt-2 items-center justify-center w-6 h-6 border-2 border-cyan-500 rounded-full">
+              <span className="text-black font-bold">6</span>
+            </div>
+            {boxes6.map((box, index) => (
+              <div
+                key={index}
+                className="border border-gray-400 rounded-md w-10 h-10 flex items-center justify-center"
+              >
+                <p className="text-xl">{box}</p>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center mb-5 ml-[-180px] max-[600px]:ml-[40px]">
+            <div className="flex mr-2 mt-2 items-center justify-center w-6 h-6 border-2 border-cyan-500 rounded-full">
+              <span className="text-black font-bold">7</span>
+            </div>
+            {boxes7.map((box, index) => (
+              <div
+                key={index}
+                className="border border-gray-400 rounded-md w-10 h-10 flex items-center justify-center"
+              >
+                <p className="text-xl">{box}</p>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center ml-[-220px] max-[600px]:ml-[40px]">
+            <div className="flex mr-2 mt-2 items-center justify-center w-6 h-6 border-2 border-cyan-500 rounded-full">
+              <span className="text-black font-bold">8</span>
+            </div>
+            {boxes8.map((box, index) => (
+              <div
+                key={index}
+                className="border border-gray-400 rounded-md w-10 h-10 flex items-center justify-center"
+              >
+                <p className="text-xl">{box}</p>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center ml-[-220px] max-[600px]:ml-[40px]">
+            <div className="flex mr-2 mt-2 items-center justify-center w-6 h-6 border-2 border-cyan-500 rounded-full">
+              <span className="text-black font-bold">9</span>
+            </div>
+            {boxes9.map((box, index) => (
+              <div
+                key={index}
+                className="border border-gray-400 rounded-md w-10 h-10 flex items-center justify-center"
+              >
+                <p className="text-xl">{box}</p>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center ml-[-220px] max-[600px]:ml-[40px]">
+            <div className="flex mr-2 mt-2 items-center justify-center w-6 h-6 border-2 border-cyan-500 rounded-full">
+              <span className="text-black font-bold">10</span>
+            </div>
+            {boxes10.map((box, index) => (
               <div
                 key={index}
                 className="border border-gray-400 rounded-md w-10 h-10 flex items-center justify-center"
@@ -90,11 +351,8 @@ export default function App() {
                     placeholder="Masukkan jawaban"
                     value={inputValue}
                     onChange={handleInputChange}
-                    className="border border-gray-400 rounded-md px-4 py-2 ml-2"
+                    className="border-2 rounded-md max-[600px]:w-[120px] max-[600px]:h-[30px] px-4 py-2 ml-2"
                   />
-                  <button type="submit" className="bg-blue-500 text-white rounded-md px-4 py-2 ml-2">
-                    Submit
-                  </button>
                 </form>
               </div>
             </div>
