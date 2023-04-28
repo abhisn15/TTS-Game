48e92919c2984d90f8465780bebb300eeea3fca1
abhisn15 <abhinugroho41@smp.belajar.id>
2023-04-28 10:42:02 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Typewriter from "typewriter-effect";
 
-function App() {
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
+import NavbarBottom from "./NavbarBottom";
+
+export default function App() {
+  const [openNav, setOpenNav] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar className="relative mx-auto max-w-screen py-2 px-4 border-4 h-32 rounded-none rounded-b-[176px] shadow-md border-cyan-400 box-border lg:px-8 lg:py-4" >
+      <div className="relative left-[106px] mx-auto items-center justify-between font-extrabold text-3xl leading-9">
+        <h1 className="title">Ayo Mainkan Teka-Teki Silang yang Menyenangkan ini!</h1>
+        <p className="relative left-28 top-3 font-normal font-medium text-base">Perhatikan soal soal tersebut dan tulis jawabannya di kolom bawah Jawabannya adalah.</p>
+      </div>
+      <MobileNav>
+      </MobileNav>
+      <NavbarBottom />
+    </Navbar>
   );
 }
-
-export default App;
